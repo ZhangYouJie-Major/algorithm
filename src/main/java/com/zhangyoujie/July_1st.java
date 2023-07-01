64f07317947cddcec072404498938b5da809adab
zhangyoujie <zhangyoujie@gigacloudtech.com>
2023-07-01 13:29:51 +0800

    算法练习 2023/7/1

--- src/main/java/com/zhangyoujie/July_1st.java
+++ src/main/java/com/zhangyoujie/July_1st.java
@@ -161,6 +161,34 @@ public class July_1st {
 
     }
 
+    /**
+     * 不同路径
+     *
+     * @param m 行
+     * @param n 列
+     * @return 路径数
+     * 动态规划 dp[i][j] = dp[i-1][j] + dp[i][j-1] dp[i][j]标识到达i,j的路径数
+     */
+    public int uniquePaths(int m, int n) {
+
+
+        int[][] dp = new int[m][n];
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
 
     public class ListNode {
         int val;
