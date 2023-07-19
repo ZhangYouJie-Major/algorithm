cf9570034bf82445fef443a940d1f440380795c6
zhangyoujie <zhangyoujie@gigacloudtech.com>
2023-07-19 17:54:34 +0800

    算法练习 2023/7/19

---
+++ src/main/java/com/zhangyoujie/July_19st.java
@@ -0,0 +1,41 @@
+package com.zhangyoujie;
+
+import java.util.Arrays;
+
+/**
+ * @author zhangyoujie
+ * @date 2023/7/19
+ */
+public class July_19st {
+
+    public static void main(String[] args) {
+        productExceptSelf(new int[]{-1,1,0,-3,3});
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+
+        int[] ans = new int[length];
+
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        //
+        L[0] = nums[0];
+        R[length - 1] = nums[length - 1];
+        for (int i = 1; i < length; i++) {
+            L[i] = L[i - 1] * nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            R[i] = R[i + 1] * nums[i];
+        }
+        ans[0] = R[1];
+        ans[length - 1] = L[length - 2];
+        for (int i = 1; i < length - 1; i++) {
+            ans[i] = L[i - 1] * R[i + 1];
+        }
+
+        return ans;
+
+    }
+}
