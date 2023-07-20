7ce04ba625daeb846a14f0383b0a701e189c0898
zhangyoujie <zhangyoujie@gigacloudtech.com>
2023-07-20 10:31:40 +0800

    算法练习 2023/7/19

--- src/main/java/com/zhangyoujie/July_19st.java
+++ src/main/java/com/zhangyoujie/July_19st.java
@@ -12,7 +12,30 @@ public class July_19st {
         productExceptSelf(new int[]{-1,1,0,-3,3});
     }
 
+
     public static int[] productExceptSelf(int[] nums) {
+
+        /*         优化代码 先计算左边的乘法 然后倒过来计算右边的乘积
+         *
+         *         int length = nums.length;
+         *         int[] ans = new int[length];
+         *         int[] L = new int[length];
+         *         int[] R = new int[length];
+         *         L[0] = nums[0];
+         *         R[length - 1] = nums[length - 1];
+         *         for (int i = 1; i < length; i++) {
+         *             L[i] = L[i - 1] * nums[i];
+         *         }
+         *         for (int i = length - 2; i >= 0; i--) {
+         *             R[i] = R[i + 1] * nums[i];
+         *         }
+         *         ans[0] = R[1];
+         *         ans[length - 1] = L[length - 2];
+         *         for (int i = 1; i < length - 1; i++) {
+         *             ans[i] = L[i - 1] * R[i + 1];
+         *         }
+         *         return ans;
+         */
         int length = nums.length;
 
         int[] ans = new int[length];
