1c83b52bd904cd20472006d9d6219d7bf03e360e
zhangyoujie <zhangyoujie@gigacloudtech.com>
2023-11-24 19:57:47 +0800

    算法练习 2023/11/24

--- src/main/java/com/zhangyoujie/Nov_24st.java
+++ src/main/java/com/zhangyoujie/Nov_24st.java
@@ -8,6 +8,38 @@ import java.util.stream.Collectors;
  * @date 2023/11/24
  */
 public class Nov_24st {
+
+    public static void main(String[] args) {
+        sortColors(new int[]{2, 0, 2, 1, 1, 0});
+    }
+
+//    public String removeDuplicateLetters(String s) {
+//
+//    }
+
+    public static void sortColors(int[] nums) {
+        int length = nums.length;
+
+        int left = 0;
+        int right = length - 1;
+        for (int i = 0; i < length; i++) {
+            while (i <= right && nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[right];
+                nums[right] = temp;
+                --right;
+            }
+
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[left];
+                nums[left] = temp;
+                ++left;
+            }
+
+        }
+    }
+
     public static int[] countBits(int n) {
         int[] ans = new int[n + 1];
         for (int i = 0; i <= n; i++) {
