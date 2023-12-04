1614216f28ad739a8a972d65690309f144c67bec
18552104642 <zhangyoujie2022@gmail.com>
2023-12-04 23:22:48 +0800

    算法练习 2023/12/03

--- src/main/java/com/zhangyoujie/dec/Dec_4st.java
+++ src/main/java/com/zhangyoujie/dec/Dec_4st.java
@@ -89,14 +89,65 @@ public class Dec_4st {
                 TreeNode treeNode = deque.pollLast();
                 if (null != treeNode) {
                     temp.add(treeNode.val);
-                    if (null != treeNode.left) deque.addFirst( treeNode.left);
-                    if (null != treeNode.right) deque.addFirst( treeNode.right);
+                    if (null != treeNode.left) deque.addFirst(treeNode.left);
+                    if (null != treeNode.right) deque.addFirst(treeNode.right);
                 }
             }
             ans.add(temp);
 
         }
         return ans;
+    }
+
+    int max = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    public int dfs(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+        //遍历当前节点的左右子树 获取两个节点相同值的数目
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        int left = 0;
+        int right = 0;
+        //如果当前节点的左节点和当前节点一致  直接+1
+        if (null != root.left && root.left.val == root.val) {
+            left = l + 1;
+        }
+        if (null != root.right && root.right.val == root.val) {
+            right = r + 1;
+        }
+        //最大值为 当前左右子树之和
+        max = Math.max(max, left + right);
+        // 当前节点的树的最大路径 返回最大的一边递归给上一层
+        return Math.max(left, right);
+    }
+
+    int minDepth = Integer.MAX_VALUE;
+
+    public int minDepth(TreeNode root) {
+        minDepthDfs(root, 0);
+        return minDepth;
+    }
+
+    public void minDepthDfs(TreeNode node, int dep) {
+        if (null != node) {
+            if (node.left == null && node.right == null) {
+                minDepth = Math.min(minDepth, dep);
+            }
+            if (null != node.left) {
+                minDepthDfs(node.left, dep + 1);
+            }
+            if (null != node.right) {
+                minDepthDfs(node.right, dep + 1);
+            }
+
+        }
     }
 }
 
