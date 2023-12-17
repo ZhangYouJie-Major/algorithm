05b03bdd771cd835fac0080e92d0591040723812
18552104642 <zhangyoujie2022@gmail.com>
2023-12-17 10:29:52 +0800

    算法练习 2023/12/10 周赛题

--- src/main/java/com/zhangyoujie/dec/Dec_14st.java
+++ src/main/java/com/zhangyoujie/dec/Dec_14st.java
@@ -1,68 +1,44 @@
 package com.zhangyoujie.dec;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-/**
- * @author zhangyoujie
- * @date 2023/12/15
- */
 public class Dec_14st {
 
-
-    private static int[][] sum;
-
     public static void main(String[] args) {
-        System.out.println(findingUsersActiveMinutes(new int[][]{{1, 1}, {2, 2}, {2, 3}}, 4));
+        System.out.println(possibleToStamp(new int[][]{{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}, 2, 2));
     }
 
-    public static int[] findingUsersActiveMinutes(int[][] logs, int k) {
-
-        int[] ans = new int[k];
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int i = 0; i < logs.length; i++) {
-            if (map.containsKey(logs[i][0])) {
-                map.get(logs[i][0]).add(logs[i][1]);
-            } else {
-                map.put(logs[i][0], new HashSet<>());
-                map.get(logs[i][0]).add(logs[i][1]);
+    public static boolean possibleToStamp(int[][] grid, int stampHeight, int stampWidth) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] sum = new int[m + 2][n + 2];
+        int[][] diff = new int[m + 2][n + 2];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + grid[i - 1][j - 1];
             }
         }
-        for (Map.Entry<Integer, Set<Integer>> entry : map.entrySet()) {
-            Set<Integer> value = entry.getValue();
-            count.put(value.size(), count.getOrDefault(value.size(), 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            ans[entry.getKey() - 1] = entry.getValue();
-        }
-        return ans;
-    }
 
-
-    private static void twoDimensionalPrefixSum(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        sum = new int[row + 1][col + 1];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                sum[i + 1][j + 1] = sum[i][j + 1] + sum[i + 1][j] - sum[i][j] + grid[i][j];
+        //计算 (x1,y1) 到 (x2,y2) 填充1  diff[x1][y1]++  diff[x2+1][y1]--  diff[x1][y2+1]--  diff[x2+1][y2+1]++
+        for (int i = 1; i + stampHeight - 1 <= m; i++) {
+            for (int j = 1; j + stampWidth - 1 <= n; j++) {
+                int x = i + stampHeight - 1;
+                int y = j + stampWidth - 1;
+                if (sum[x][y] - sum[x][j - 1] - sum[i - 1][y] + sum[i - 1][j - 1] == 0) {
+                    diff[i][j]++;
+                    diff[i][y + 1]--;
+                    diff[x + 1][j]--;
+                    diff[x + 1][y + 1]++;
+                }
             }
         }
-    }
 
-    /**
-     * 查询左上角为x1,y1 右下角为x2-1,y2-1的矩形区域的和
-     *
-     * @param x1
-     * @param y1
-     * @param x2
-     * @param y2
-     * @return
-     */
-    private static int query(int x1, int y1, int x2, int y2) {
-        return sum[x2][y2] - sum[x2][y1] - sum[x1][y2] + sum[x1][y1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
+                if (diff[i][j] == 0 && grid[i - 1][j - 1] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
