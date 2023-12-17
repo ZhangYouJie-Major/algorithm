5a1e67e802bba52c3ff66df2b7bb58cc09cb9c9b
18552104642 <zhangyoujie2022@gmail.com>
2023-12-17 14:24:14 +0800

    算法练习 2023/12/17 周赛题

---
+++ src/main/java/com/zhangyoujie/dec/Dec_17st.java
@@ -0,0 +1,45 @@
+package com.zhangyoujie.dec;
+
+import java.util.*;
+
+public class Dec_17st {
+
+    public static int[][] divideArray(int[] nums, int k) {
+        int length = nums.length;
+        Arrays.sort(nums);
+        int[][] ans = new int[length / 3][3];
+        for (int i = 0; i < length; i += 3) {
+            if (nums[i + 1] - nums[i] > k || nums[i + 2] - nums[i] > k) {
+                ans = new int[0][0];
+                break;
+            }
+            ans[i / 3] = new int[]{nums[i], nums[i + 1], nums[i + 2]};
+        }
+        return ans;
+    }
+
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        Set<Integer> set = new HashSet<>();
+        int ans1 = 0;
+        int ans2 = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (set.contains(grid[i][j])) {
+                    ans1 = grid[i][j];
+                } else {
+                    set.add(grid[i][j]);
+                }
+            }
+        }
+        for (int i = 1; i <= row * row; i++) {
+            if (!set.contains(i)) {
+                ans2 = i;
+            }
+        }
+        return new int[]{ans1, ans2};
+    }
+
+}
